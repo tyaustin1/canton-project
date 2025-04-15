cf8c3bd7a9c1b9a0ed5a423314b621e9fe4cc557
Ty <ty@Tys-MacBook-Pro.hsd1.mi.comcast.net>
2025-04-15 01:58:33 -0400

    More changes

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Hello world
+            Hello world, Deployed by Ty
           </li>
         </ol>
 
