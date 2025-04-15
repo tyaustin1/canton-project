ad9c96a3deae853394cf48921a68bf52c054922c
Ty <ty@Tys-MacBook-Pro.hsd1.mi.comcast.net>
2025-04-15 01:45:03 -0400

    Test html change

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Hello world, from Ford
+            Hello world, from Ford Credit
           </li>
         </ol>
 
