ec452af239d18ffde990892dcb69743489c37ae7
rodisho <rafiodisho@gmail.com>
2025-04-22 00:49:56 -0400

    feat: more refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
 export default function Home() {
   return (
       <div className="container">
-        <div className="section">
-          {/* Section 1 Content */}
-          Team A
+        <div className="row">
+          <div className="section">
+            {/* Section 1 Content */}
+            Team A
+          </div>
+          <div className="section">
+            {/* Section 2 Content */}
+            Team B
+          </div>
         </div>
-        <div className="section">
-          {/* Section 2 Content */}
-          Team B
-        </div>
-        <div className="section">
-          {/* Section 3 Content */}
-          Team C
-        </div>
-        <div className="section">
-          {/* Section 4 Content */}
-          Team D
+        <div className="row">
+          <div className="section">
+            {/* Section 3 Content */}
+            Team C
+          </div>
+          <div className="section">
+            {/* Section 4 Content */}
+            Team D
+          </div>
         </div>
       </div>
   );
