b25f8f5c34de21f7a2f00c189d625f9267a2b331
rodisho <rafiodisho@gmail.com>
2025-04-22 00:29:34 -0400

    feat: extract css

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
-      <div className="flex h-screen w-screen">
-        <div className="w-1/4 h-full flex items-center justify-center border">
+      <div className="container">
+        <div className="section">
           {/* Section 1 Content */}
           Section 1
         </div>
-        <div className="w-1/4 h-full flex items-center justify-center border">
+        <div className="section">
           {/* Section 2 Content */}
           Section 2
         </div>
-        <div className="w-1/4 h-full flex items-center justify-center border">
+        <div className="section">
           {/* Section 3 Content */}
           Section 3
         </div>
-        <div className="w-1/4 h-full flex items-center justify-center border">
+        <div className="section">
           {/* Section 4 Content */}
           Section 4
         </div>
