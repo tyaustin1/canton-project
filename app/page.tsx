aea276518f831bbb408ecf8c95be1d8080fddd10
rodisho <rafiodisho@gmail.com>
2025-04-22 01:19:38 -0400

    feat: leverage different API for displaying the dog image

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,33 @@
-async function getRandomImage() {
-  const response = await fetch("https://source.unsplash.com/random?team", {
-    redirect: 'follow' // Ensure redirects are followed
-  });
-
-  const imageUrl = response.url;
-
-  return imageUrl;
+async function getRandomDogImage() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
+  return data.message;
 }
 
 export default async function Home() {
-  const imageUrl = await getRandomImage();
+  const imageUrl = await getRandomDogImage();
 
   return (
       <div className="container">
         <div className="row">
-          <div className="section" style={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            color: 'white', // Adjust text color for visibility
-            textAlign: 'center',
-          }}>
+          <div
+              className="section"
+              style={{
+                backgroundImage: `url(${imageUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                color: "white",
+                textAlign: "center",
+              }}
+          >
             {/* Section 1 Content */}
             Team A
           </div>
-          <div className="section">
-            {/* Section 2 Content */}
-            Team B
-          </div>
+          <div className="section">{/* Section 2 Content */}Team B</div>
         </div>
         <div className="row">
-          <div className="section">
-            {/* Section 3 Content */}
-            Team C
-          </div>
-          <div className="section">
-            {/* Section 4 Content */}
-            Team D
-          </div>
+          <div className="section">{/* Section 3 Content */}Team C</div>
+          <div className="section">{/* Section 4 Content */}Team D</div>
         </div>
       </div>
   );
