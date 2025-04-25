da76bcf82bfeebfc40bf253a89b42b55132dd346
rodisho <rafiodisho@gmail.com>
2025-04-25 15:51:23 -0400

    updating cat images

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,36 @@
 import { useEffect, useState } from "react";
 
 async function getRandomDogImage() {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  const data = await response.json();
-  return data.message;
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+    return data.fact;
 }
 
 export default function Home() {
-  const [imageUrl, setImageUrl] = useState("");
+    const [fact, setFact] = useState("");
 
-  useEffect(() => {
-    const fetchImage = async () => {
-      const image = await getRandomDogImage();
-      setImageUrl(image);
-    };
-    fetchImage();
-  }, []);
-  return (
-      <div className="container">
-        <div className="row">
-          <div
-              className="section"
-              style={{
-                backgroundImage: `url(${imageUrl})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                color: "white",
-                textAlign: "center",
-              }}
-          >
-            {/* Section 1 Content */}
-            Team A
-          </div>
-          <div className="section">{/* Section 2 Content */}Hello Canton from Team B</div>
+    useEffect(() => {
+        const fetchFact = async () => {
+            const newFact = await getRandomDogImage();
+            setFact(newFact);
+        };
+
+        fetchFact();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="section">
+                    {/* Section 1 Content */}
+                    {fact}
+                </div>
+                <div className="section">{/* Section 2 Content */}Hello Canton from Team B</div>
+            </div>
+            <div className="row">
+                <div className="section">{/* Section 3 Content */}Team C</div>
+                <div className="section">{/* Section 4 Content */}Team D</div>
+            </div>
         </div>
-        <div className="row">
-          <div className="section">{/* Section 3 Content */}Team C</div>
-          <div className="section">{/* Section 4 Content */}Team D</div>
-        </div>
-      </div>
-  );
+    );
 }
